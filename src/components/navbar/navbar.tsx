c24a1206bb810301b55e8121947e467b219a1cac
z5362831 <z5362831@ad.unsw.edu.au>
2025-05-27 22:11:51 +1000

    creating menus for navbar

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -2,6 +2,65 @@ import React from "react";
 import companyLogo from "@/assets/company-logo-navbar.svg";
 import ThemeToggle from "./theme-toggle";
 import { Link } from "react-router-dom";
+import { Users, BookOpenText, BriefcaseBusiness } from "lucide-react";
+
+interface MenuItem {
+  title: string
+  url: string
+  description?: string
+  icon?: React.ReactNode
+  items?: MenuItem[]
+}
+
+interface Logo {
+  url: string
+  src: string
+  alt?: string
+  title: string
+}
+
+interface NavBarProps {
+  logo?: Logo
+  menu?: MenuItem[]
+}
+
+const logo: Logo = {
+  url: "/",
+  src: companyLogo,
+  title: "E Union Communication"
+}
+
+const homeMenu: MenuItem = {
+  title: "Home",
+  url: "/",
+}
+const aboutMenu: MenuItem = {
+  title: "About",
+  url: "/about",
+  items: [
+    {
+      title: "The Team",
+      url: "###",
+      description: "Meet the team behind the company",
+      icon: <Users className="size-5 shrink-0" />
+    },
+    {
+      title: "Our Story",
+      url: "###",
+      description: "Learn about our journey and values",
+      icon: <BookOpenText className="size-5 shrink-0" />
+    },
+    {
+      title: "Careers",
+      url: "###",
+      description: "Join our team and grow with us",
+      icon: <BriefcaseBusiness className="size-5 shrink-0" />
+    },
+  ]
+}
+
+
+
 
 const linkStyle = "font-medium text-white transition-colors transition-transform duration-200 hover:text-[var(--color-accent)] hover:scale-108 active:animate-tilt-shaking"
 
@@ -9,18 +68,17 @@ const linkStyle = "font-medium text-white transition-colors transition-transform
 const NavBar: React.FC = () => {
 	return (
     <nav className="flex items-center justify-between space-x-6 px-6 py-4 bg-[#7F8CAA] w-full top-0">
-      {/* Logo */}
       <div className="flex items">
         <img src={companyLogo} className="h-10 w-auto" />
       </div>
-      {/* Navigation Links */}
+
       <div className="flex space-x-6">
-		<Link to="/" className={linkStyle}>Home</Link>
-		<Link to="/about" className={linkStyle}>About</Link>
-		<Link to="/services" className={linkStyle}>Services</Link>
-		<Link to="/contact" className={linkStyle}>Contact</Link>
+        <Link to="/" className={linkStyle}>Home</Link>
+        <Link to="/about" className={linkStyle}>About</Link>
+        <Link to="/services" className={linkStyle}>Services</Link>
+        <Link to="/contact" className={linkStyle}>Contact</Link>
       </div>
-      {/* Phone Number */}
+
       <div className="flex items-center space-x-6">
         <ThemeToggle />
         <a href="tel:0123456789">
